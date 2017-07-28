cf6c93f7b84f8e3ab781d5bfd870a29a37198cef
Paulo Henrique Martins <paulo.martins@savedo.de>
2017-07-28 12:26:37 +0200

    v 0.1

---
+++ README.md
@@ -0,0 +1,49 @@
+# AGERTEK
+## QCS queue simulator
+
+Simulating business rules for queue system.
+
+---
+### how it works?
+
+#### to add a new element:
+- if it's a common one (type `a`), just adds to the end of the queue.
+- if it's a special element (types `b` or `c`), it is added after two consecutives `a` elements
+
+#### code sample:
+```
+function newInQueue(el) {
+  switch(el) {
+    case "b":
+    case "c":
+      addAfterXAfegaos(el, 2);
+      break;
+    case "a":
+    default:
+      queue.push(el);
+  }
+  buildQueue();
+}
+
+function addAfterXAfegaos(element, afterX) {
+  var afgs = 0;
+  for (var i = 0; i < queue.length; i++) {
+    var type = queue[i];
+    if (type == "a") afgs ++;
+    else afgs = 0;
+    var nextInQueue = queue[i+1];
+    if (afgs == afterX){
+      if(nextInQueue == "a") break;
+    }
+  }
+  i++;
+  console.info("adding "+element+" at position " + i);
+  queue.splice(i, 0, element);
+}
+```
+
+- For special elements:
+I check whenever I have two consecutive `a` elements. If I do, I check the next in queue - if it's also an `a`, I add the new element there.
+
+
+
---
+++ script.js
@@ -0,0 +1,64 @@
+var queue = [];
+
+$(document).ready(function(){
+  console.info("loading...");
+  startQueue();
+});
+
+function startQueue() {
+  queue.push("a");
+  queue.push("a");
+  queue.push("a");
+  queue.push("a");
+  queue.push("a");
+  buildQueue();
+}
+
+function newInQueue(el) {
+  switch(el) {
+    case "b":
+    case "c":
+      addAfterXAfegaos(el, 2);
+      break;
+    case "a":
+    default:
+      queue.push(el);
+  }
+  buildQueue();
+}
+
+function addAfterXAfegaos(element, afterX) {
+  var afgs = 0;
+  for (var i = 0; i < queue.length; i++) {
+    var type = queue[i];
+    if (type == "a") afgs ++;
+    else afgs = 0;
+    var nextInQueue = queue[i+1];
+    if (afgs == afterX){
+      if(nextInQueue == "a") break;
+    }
+  }
+  i++;
+  console.info("adding "+element+" at position " + i);
+  queue.splice(i, 0, element);
+}
+
+function neeext() {
+  queue.shift();
+  buildQueue();
+}
+
+function buildQueue() {
+  console.info("building queue", queue);
+  getQueueContainer().empty();
+  for (var i = 0; i < queue.length; i++) {
+    var q = queue[i];
+    var user = document.createElement("span");
+    user.className = "user user-"+q;
+    getQueueContainer().append(user);
+  }
+}
+
+function getQueueContainer() {
+  return $(".queue-show");
+}
